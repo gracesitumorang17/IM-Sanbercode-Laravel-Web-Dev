b6338fdb13eb0a6b8bd8fdec441d28cc7e1686c7
gracecalderasitumorang <143679212+gracesitumorang17@users.noreply.github.com>
2025-08-22 17:20:09 +0700

    Tugas 14

---
+++ reviewbook/database/migrations/2025_08_22_074055_create_books_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('summary');
+            $table->string('image');
+            $table->integer('stok');
+            $table->unsignedBigInteger('gendre_id');
+ 
+             $table->foreign('gendre_id')->references('id')->on('gendres');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
